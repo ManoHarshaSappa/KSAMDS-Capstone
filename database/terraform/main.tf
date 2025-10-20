0f75d16cc8430e88be6f08f5aa70b5afb755aa53
Mano Harsha Sappa <160296860+ManoHarshaSappa@users.noreply.github.com>
2025-10-19 22:08:40 -0400

    Update main.tf

--- database/terraform/main.tf
+++ database/terraform/main.tf
@@ -105,7 +105,7 @@ resource "aws_db_subnet_group" "ksamds" {
 resource "aws_db_instance" "ksamds" {
   identifier           = "ksamds-db"
   engine              = "postgres"
-  engine_version      = "15.3"
+  engine_version      = "17.4"
   instance_class      = "db.t3.micro"  # Free tier eligible
   allocated_storage   = 20
   storage_type        = "gp2"
